9c718f6790ff064dd12494c1a89e7522aaeb9fd5
John Doe <johndoe@example.com>
2025-01-23 12:04:27 -0300

    finalizando-perfil

--- models/perfil.ts
+++ models/perfil.ts
@@ -1,3 +1,5 @@
+import { Publicacao } from "./publicacao";
+
 export class Perfil {
   private _idUnico: number;
   private _apelido: string;
@@ -5,54 +7,51 @@ export class Perfil {
   private _stats: boolean;
   private _amigos: Perfil[];
   private _foto: string;
-  //private _postagens: Publicacao[]
+  private _publicacoes: Publicacao[];
 
-  constructor(apelido: string, email: string, foto: string) {
+  constructor(id: number, apelido: string, email: string, foto: string) {
+    this._idUnico = id;
     this._apelido = apelido;
     this._email = email;
+    this._foto = foto;
     this._stats = true;
     this._amigos = [];
-    this._foto = foto;
-  }
-
-  public set id(id: number) {
-    this._idUnico = id;
+    this._publicacoes = [];
   }
 
   public get id(): number {
     return this._idUnico;
   }
-
-  public set apelido(apelido: string) {
-    this._apelido = apelido;
+  public set id(value: number) {
+    this._idUnico = value;
   }
 
   public get apelido(): string {
     return this._apelido;
   }
-
-  public set email(email: string) {
-    this._email = email;
+  public set apelido(value: string) {
+    this._apelido = value;
   }
 
   public get email(): string {
     return this._email;
   }
-
-  public set stats(stats: boolean) {
-    this._stats = stats;
+  public set email(value: string) {
+    this._email = value;
   }
 
   public get stats(): boolean {
     return this._stats;
   }
-
-  public set amigos(amigos: Perfil[]) {
-    this._amigos = amigos;
+  public set stats(value: boolean) {
+    this._stats = value;
   }
 
   public get amigos(): Perfil[] {
     return this._amigos;
+  }
+  public set amigos(value: Perfil[]) {
+    this._amigos = value;
   }
 
   public adicionarAmigo(amigo: Perfil): void {
@@ -65,19 +64,45 @@ export class Perfil {
     );
   }
 
-  public listarCopiaDeAmigos(): Perfil[] {
+  public listarAmigos(): Perfil[] {
     let copiaDeAmigos: Perfil[] = [];
 
     for (let amigo of this._amigos) {
-      let perfilCopiado = new Perfil(amigo._apelido, amigo._email, amigo._foto);
-      perfilCopiado.id = amigo.id;
+      let perfilCopiado = new Perfil(
+        amigo._idUnico,
+        amigo._apelido,
+        amigo._email,
+        amigo._foto
+      );
       copiaDeAmigos.push(perfilCopiado);
     }
 
     return copiaDeAmigos;
   }
 
-  //public adicionarPublicacao(){}
+  public adicionarPublicacao(publicacao: Publicacao): void {
+    this._publicacoes.push(publicacao);
+  }
 
-  //public listarPublicacao(){}
+  public removerPublicacao(id: number): void {
+    this._publicacoes = this._publicacoes.filter(
+      (publicacoes) => publicacoes.id !== id
+    );
+  }
+
+  public listarPublicacao(): Publicacao[] {
+    let publicacoesCopiadas: Publicacao[] = [];
+
+    for (let publicacao of this._publicacoes) {
+      let copiaPub = new Publicacao(
+        publicacao.id,
+        publicacao.conteudo,
+        publicacao.dataHora,
+        publicacao.perfilAssociado
+      );
+      publicacoesCopiadas.push(copiaPub);
+    }
+
+    return publicacoesCopiadas;
+  }
 }
--- models/perfilAvancado.ts
+++ models/perfilAvancado.ts
@@ -1,21 +1,23 @@
-import { Perfil } from "./perfil.ts"
+import { Perfil } from "./perfil.ts";
 
 class PerfilAvancado extends Perfil {
-    constructor(apelido: string, email: string, foto: string) {
-        super(apelido, email, foto)
+  constructor(id: number, apelido: string, email: string, foto: string) {
+    super(id, apelido, email, foto);
 
-        this.stats = true
-        this.amigos = []
-    }
+    this.stats = true;
+    this.amigos = [];
+  }
 
-    public habDesabPerfis(apelido: string): void{
-        let perfilBuscado: Perfil | undefined = super.amigos.find(perfil => perfil.apelido === apelido) 
-    
-        if(perfilBuscado) {
-            perfilBuscado.stats = false
-            console.log("Perfil desativado.")
-        }else {
-            console.log("Perfil não encontrado.")
-        }
+  public habDesabPerfis(apelido: string): void {
+    let perfilBuscado: Perfil | undefined = super.amigos.find(
+      (perfil) => perfil.apelido === apelido
+    );
+
+    if (perfilBuscado) {
+      perfilBuscado.stats = false;
+      console.log("Perfil desativado.");
+    } else {
+      console.log("Perfil não encontrado.");
     }
+  }
 }
