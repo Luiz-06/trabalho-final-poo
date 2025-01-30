1c929725aa055cdd873b30dd02399dcc1aadad8e
John Doe <johndoe@example.com>
2025-01-30 11:24:33 -0300

    gerenciamento de publicacoes

--- models/redeSocial.ts
+++ models/redeSocial.ts
@@ -5,6 +5,8 @@ import {
 import { Perfil } from "./perfil";
 import { Publicacao } from "./publicacao";
 import { PublicacaoAvancada } from "./publicacaoAvancada";
+import { TipoInteracao } from "./tipoInteracao";
+import { Interacao } from "./interacao";
 
 export class RedeSocial {
   private _perfisCadastrados: Perfil[];
@@ -50,9 +52,8 @@ export class RedeSocial {
     }
   }
 
-  //PUBLICACOES
   private deletarPublicacao(perfilRemovido: Perfil): void {
-    this._publicacoesPostadas.filter(
+    this._publicacoesPostadas = this._publicacoesPostadas.filter(
       (postagem) => postagem.perfilAssociado.id !== perfilRemovido.id
     );
   }
@@ -97,23 +98,67 @@ export class RedeSocial {
   public desativarPerfil(apelido: string): void {
     let perfilProcurado = this.buscarPerfil(apelido);
 
-    if (perfilProcurado?.stats == false) {
-      perfilProcurado.stats = true;
+    if (perfilProcurado?.stats == true) {
+      perfilProcurado.stats = false;
     } else {
       console.log("Perfil já se encontra desativado.");
     }
   }
 
-  //SOLICITACOES
+  //PUBLICACOES
+  private estaAssociada(publicacao: Publicacao): boolean {
+    return publicacao.perfilAssociado ? true : false;
+  }
+
+  public adicionarPublicacao(publicacao: Publicacao): void {
+    if (!this.estaAssociada(publicacao)) {
+      console.log("Esta publicacao não está associada a nenhum perfil");
+      return;
+    }
+    this._publicacoesPostadas.push(publicacao);
+  }
+
+  public listarPublicacoes(apelido?: string): Publicacao[] {
+    let publicacoesFiltradas = apelido
+      ? this._publicacoesPostadas.filter(
+          (publicacao) => publicacao.perfilAssociado.apelido === apelido
+        )
+      : this._publicacoesPostadas;
+
+    publicacoesFiltradas.sort((a, b) => {
+      const dataA = new Date(a.dataHora);
+      const dataB = new Date(b.dataHora);
+      return dataB.getTime() - dataA.getTime(); 
+    });
+
+    return publicacoesFiltradas;
+  }
+
+  //INTERAÇÕES 
+
+  // SOLICITAÇÕES DE AMIZADE
   public enviarSolicitacaoAmizade(
     apelidoRemetente: string,
     apelidoDestinatario: string
   ): void {
-    let perfilRemetente = this.buscarPerfil(apelidoRemetente);
-    let perfilDestinatario = this.buscarPerfil(apelidoDestinatario);
+    let remetente = this.buscarPerfil(apelidoRemetente);
+    let destinatario = this.buscarPerfil(apelidoDestinatario);
 
-    if (perfilRemetente && perfilDestinatario) {
-      perfilDestinatario.addCaixaDeSolicitacoes(perfilRemetente);
+    if (remetente && destinatario) {
+      destinatario.addCaixaDeSolicitacoes(remetente);
+    }
+  }
+
+  public aceitarSolicitacaoAmizade(
+    apelidoRemetente: string,
+    apelidoDestinatario: string,
+    aceitar: boolean
+  ): void {
+    let destinatario = this.buscarPerfil(apelidoDestinatario);
+    let remetente = this.buscarPerfil(apelidoRemetente);
+
+    if (remetente && destinatario) {
+      destinatario.aceitarSolicitacao(remetente, aceitar);
     }
   }
 }
