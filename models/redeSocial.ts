104746a4786440bb10e07cfcab2413618e024ea6
Luiz Felipe <fogueteb8@gmail.com>
2025-02-02 11:34:10 -0300

    publicacoes em rede-social

--- models/redeSocial.ts
+++ models/redeSocial.ts
@@ -98,20 +98,54 @@ export class RedeSocial {
   public desativarPerfil(apelido: string): void {
     let perfilProcurado = this.buscarPerfil(apelido);
 
-    if(perfilProcurado?.stats == false){
-        perfilProcurado.stats = true
-    }else {
-        console.log("Perfil já se encontra desativado.")
+    if (perfilProcurado?.stats == true) {
+      perfilProcurado.stats = false;
+    } else {
+      console.log("Perfil já se encontra desativado.");
     }
   }
 
-  //SOLICITACOES
-  public enviarSolicitacaoAmizade(apelidoRemetente: string, apelidoDestinatario: string): void{
-    let perfilRemetente = this.buscarPerfil(apelidoRemetente);
-    let perfilDestinatario = this.buscarPerfil(apelidoDestinatario);
+  //PUBLICACOES
+  private estaAssociada(publicacao: Publicacao): boolean {
+    return publicacao.perfilAssociado ? true : false;
+  }
 
-    if (perfilRemetente && perfilDestinatario) {
-      perfilDestinatario.addCaixaDeSolicitacoes(perfilRemetente);
+  public adicionarPublicacao(publicacao: Publicacao): void {
+    if (!this.estaAssociada(publicacao)) {
+      console.log("Esta publicacao não está associada a nenhum perfil");
+      return;
+    }
+    this._publicacoesPostadas.push(publicacao);
+  }
+
+  public listarPublicacoes(apelido?: string): Publicacao[] {
+    let publicacoesFiltradas = apelido
+      ? this._publicacoesPostadas.filter(
+          (publicacao) => publicacao.perfilAssociado.apelido === apelido
+        )
+      : this._publicacoesPostadas;
+
+    publicacoesFiltradas.sort((a, b) => {
+      const dataA = new Date(a.dataHora);
+      const dataB = new Date(b.dataHora);
+      return dataB.getTime() - dataA.getTime(); 
+    });
+
+    return publicacoesFiltradas;
+  }
+
+  //INTERAÇÕES 
+
+  // SOLICITAÇÕES DE AMIZADE
+  public enviarSolicitacaoAmizade(
+    apelidoRemetente: string,
+    apelidoDestinatario: string
+  ): void {
+    let remetente = this.buscarPerfil(apelidoRemetente);
+    let destinatario = this.buscarPerfil(apelidoDestinatario);
+
+    if (remetente && destinatario) {
+      destinatario.addCaixaDeSolicitacoes(remetente);
     }
   }
 
